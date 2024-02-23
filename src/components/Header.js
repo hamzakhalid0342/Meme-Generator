bcad67b9a3b072ca8aa30d5a7bc75c5abc00beac
Hamza Khalid <117151844+hamzakhalid0342@users.noreply.github.com>
2024-02-24 01:43:16 +0500

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <>
+    <div className="header">
+    <img src='./troll-face.png' alt='' className='header--image'/>
+    <h1 className='header--title'> Meme Generator</h1>
+    <p className='header--creator'>Created by Hamza Khalid</p>   
+    </div>
+
+     
+
+    </>
+    
+  )
+}
